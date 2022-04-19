db9b4d0330e09e75db9e85bb5821403b010a8b01
Wei-Yun <flyling503@gmail.com>
2022-04-19 20:40:29 +0800

    feat(post.js): hide versionkey

--- models/post.js
+++ models/post.js
@@ -1,43 +1,48 @@
 const mongoose = require('mongoose')
 
-const postSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, '姓名未填寫']
-  },
-  tags: [
-    {
+const postSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: [true, 'tags 未填寫']
+      required: [true, '姓名未填寫']
+    },
+    tags: [
+      {
+        type: String,
+        required: [true, 'tags 未填寫']
+      }
+    ],
+    type: {
+      type: String,
+      enum: ['group', 'person'],
+      required: [true, 'type 未填寫']
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      select: false
+    },
+    content: {
+      type: String,
+      required: [true, 'Content 未填寫'],
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
     }
-  ],
-  type: {
-    type: String,
-    enum:['group','person'],
-    required: [true, 'type 未填寫']
   },
-  image: {
-    type: String,
-    default: ''
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-    select: false
-  },
-  content: {
-    type: String,
-    required: [true, 'Content 未填寫'],
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments:{
-    type: Number,
-    default: 0
+  {
+    versionKey: false
   }
-})
+)
 
 const Post = mongoose.model('Post', postSchema)
 
